e56929fcda8cd203ae58f499b09db0653d9ec9e7
omovo <omovohtennison@gmail.com>
2023-08-08 02:56:14 +0100

    Header Complete

---
+++ .history/src/components/Header_20230808004704.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+        <div className="main-info">
+        
+        <h1>Welcome To Winners Chapel International Peterborough</h1>
+        <Typed/>
+        
+        
+        </div>
+        </div>
+    
+    )
+}
+
+export default Header
